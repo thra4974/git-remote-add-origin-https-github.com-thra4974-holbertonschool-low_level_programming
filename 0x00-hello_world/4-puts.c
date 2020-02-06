1c21d607b0a332e869c937b5df23ab313fe78d0e
thra4974 <tristan.thrasher-1@ou.edu>
2020-02-05 21:10:01 -0600

    4-puts added

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char string[50];
+
+	strcpy(string, "\"Programming is like building a multilingual puzzle");
+
+	puts(string);
+
+	return (0);
+}
