724eb36d3828c236e216704e96d8e64ff0fb8595
thra4974 <tristan.thrasher-1@ou.edu>
2020-02-06 16:46:28 -0600

    Added README and task 0 done

---
+++ 0x00-hello_world/blog.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+
+/*This is a comment. It will be ignored during preprocess*/
+
+int main(void)
+
+{
+	printf("What a process");
+	return(0);
+}
+
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+Variables, if-else, and while loops in C project for Holberton School
