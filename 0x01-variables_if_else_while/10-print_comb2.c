e644022b093b4863f70805ae7d9cd7cc6bdb1393
thra4974 <tristan.thrasher-1@ou.edu>
2020-02-07 00:16:21 -0600

    task 10 added

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+
+	for (int i = 0; i < 100 ; i++)
+
+	{
+		putchar((i / 10) + '0');
+
+		putchar((i % 10) + '0');
+
+		putchar(44);
+
+		putchar(32);
+
+	}
+
+	return (0);
+
+}
