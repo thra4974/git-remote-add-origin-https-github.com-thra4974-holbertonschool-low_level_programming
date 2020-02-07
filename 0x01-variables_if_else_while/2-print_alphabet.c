18acd9dace1ca8c75a66de55fb6f29c7bbaabde5
thra4974 <tristan.thrasher-1@ou.edu>
2020-02-06 19:33:33 -0600

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+
+	char ch = 'a';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+
+	{
+		putchar(ch);
+	}
+
+	return (0);
+
+}
