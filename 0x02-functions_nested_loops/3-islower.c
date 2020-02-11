88c0c7cbafbd081407cd19c41a6f8102318aba91
thra4974 <tristan.thrasher-1@ou.edu>
2020-02-11 14:09:00 -0600

    added task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _islower - checks for lowercase character
+ *
+ * 
+ */
+
+int _islower(int c)
+
+{
+	if (c >= 97 && c <= 122)
+		
+	{
+		return (1);
+	}
+	
+	else {
+		return (0);
+	}
+}
