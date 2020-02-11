45bf6938fa45fe3b06c70b78407aed5495d7c760
thra4974 <tristan.thrasher-1@ou.edu>
2020-02-11 14:15:38 -0600

    fixed task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,19 +3,20 @@
 /**
  * _islower - checks for lowercase character
  *
- * 
+ * Return: Always 1
+ * @c: parameter for _islower
  */
 
 int _islower(int c)
 
 {
 	if (c >= 97 && c <= 122)
-		
-	{
+
+
 		return (1);
-	}
-	
-	else {
+
+
+	else
 		return (0);
-	}
+
 }
