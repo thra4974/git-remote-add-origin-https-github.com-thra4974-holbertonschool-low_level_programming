4228f06c42bbb6548921013e569d08329c0c5ed3
thra4974 <tristan.thrasher-1@ou.edu>
2020-02-11 18:54:49 -0600

    task 0 fixed

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -1,8 +1,8 @@
-int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int n);
-int print_last_digit(int i);
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+
+#endif /* HOLBERTON_H */
