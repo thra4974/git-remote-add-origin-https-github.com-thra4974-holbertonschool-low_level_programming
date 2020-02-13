59b364dc2f817e15bade59101117bdfa0e716886
thra4974 <tristan.thrasher-1@ou.edu>
2020-02-13 10:34:16 -0600

    program to check for upper added

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,21 +8,9 @@
 
 void times_table(void)
 {
-	int tim0 = n * 0;
-	int tim1 = n * 1;
-	int tim2 = n * 2;
-	int tim3 = n * 3;
-	int tim4 = n * 4;
-	int tim5 = n * 5;
-	int tim6 = n * 6;
-	int tim7 = n * 7;
-	int tim8 = n * 8;
-	int tim9 = n * 9; 
+	int top;
+	int side;
 
-	for (n = 0; n <= 9; n++)
-
-	{
-		_putchar(tim1);
-	}
+	
 
 }
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,4 @@
+<h1> More Functions and Nested Loops </n1>
+
+1: Write a function that checks for uppercase letters.
+2:
