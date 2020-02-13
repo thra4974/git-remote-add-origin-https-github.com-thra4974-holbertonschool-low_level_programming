21ce107db5589e9d61bd2155e8b2bcb4ce314ebe
thra4974 <tristan.thrasher-1@ou.edu>
2020-02-13 12:51:12 -0600

    print numbers 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - print numbers 0-9.
+ * Return: void.
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+
+		_putchar(n);
+	}
+
+	_putchar('\n');
+
+
+}
