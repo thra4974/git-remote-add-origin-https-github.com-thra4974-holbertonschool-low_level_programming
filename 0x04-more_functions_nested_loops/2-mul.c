b368a406d2f072a74076003ec205f006dc9a1b89
thra4974 <tristan.thrasher-1@ou.edu>
2020-02-13 11:22:49 -0600

    Program returns product of two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * mul - multiplies two integers (a, b).
+ * Return: mul.
+ * @a: first integer to be multiplied.
+ * @b: second integer to be multiplied.
+ */
+
+int mul(int a, int b)
+{
+	int mult = a * b;
+
+	return (mult);
+
+}
