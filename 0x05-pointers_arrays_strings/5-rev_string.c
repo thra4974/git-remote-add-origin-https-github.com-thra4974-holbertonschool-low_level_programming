71b5138921dd71af45efc53a2e1b8af23c1bc721
thra4974 <tristan.thrasher-1@ou.edu>
2020-02-18 16:24:41 -0600

    print evens

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * rev_string - write a function that reverses a string.
+ * @s: string to be reversed.
+ * Return: nothing, 
+ */
+
+void rev_string(char *s)
+{
+	
+}
---
+++ 0x05-pointers_arrays_strings/6-main.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+#include <stdio.h>
+
+int main(void)
+{
+	char *str;
+
+	str = "0123456789";
+	puts2(str);
+	return (0);
+}
