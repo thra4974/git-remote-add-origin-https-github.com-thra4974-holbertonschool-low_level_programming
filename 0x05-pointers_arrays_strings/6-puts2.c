f406a73ec2d27dc76f9fe8a5f36c4c910176525a
thra4974 <tristan.thrasher-1@ou.edu>
2020-02-18 19:28:42 -0600

    6 updated

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,14 +9,16 @@
 void puts2(char *str)
 {
 	int i = 0;
+	int j = 0;
 
 	while (str[i] != '\0')
 	{
-		if (str[i] % 2 == 0)
-		{
-			_putchar(str[i]);
-		}
 		i++;
+	}
+	while (j < i)
+	{
+		_putchar(str[j]);
+		j = j + 2;
 	}
 	_putchar('\n');
 }
