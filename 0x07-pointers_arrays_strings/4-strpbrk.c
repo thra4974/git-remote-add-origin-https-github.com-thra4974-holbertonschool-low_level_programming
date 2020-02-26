a02166580841038f178922917c2a4bcecf94498b
thra4974 <tristan.thrasher-1@ou.edu>
2020-02-25 19:14:13 -0600

    task 4 finished

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -4,21 +4,22 @@
  * _strpbrk - locates first occuences in s of bytes in accept.
  * @s: string to be evaluated.
  * @accept: contains bytes to be located in s.
+ * Return: Character 's'.
  */
 
 char *_strpbrk(char *s, char *accept)
 {
-	int i = 0;
 	int j = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (*s++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
-			if (s[i] == accept[j])
+			if (*s == accept[j])
 			{
 				return (s);
 			}
+
 		}
 
 	}
