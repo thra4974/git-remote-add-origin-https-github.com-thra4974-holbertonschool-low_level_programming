3d6e31b5f02963d7aeb031125c9e97c66ccd2c24
thra4974 <tristan.thrasher-1@ou.edu>
2020-02-26 23:56:45 -0600

    update

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _sqrt_recursion - function returns natural square root.
+ * @n: integer to be evaluated by function.
+ * Return: integer.
+ */
+
+int _sqrt_recursion(int n)
+{
+
+	
+	if ( )
+	{
+		return (-1);
+	}
+
+	else
+	{
+		return (x * (_sqrt_recursion(x)
+}
