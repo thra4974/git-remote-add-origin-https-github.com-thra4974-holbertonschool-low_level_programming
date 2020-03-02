dd438ebefe381fc43c4480cd3b5b1567ce70d046
thra4974 <tristan.thrasher-1@ou.edu>
2020-03-01 18:38:37 -0600

    fixed task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,19 +12,20 @@
 
 int main(int argc, char *argv[])
 {
-	int a = atoi(argv[1]);
-	int b = atoi(argv[2]);
-	int res = a * b;
-
-	if (argc == 3)
-	{
-		printf("%d\n", res);
-	}
-
-	else
+	if (argc != 3)
 	{
 		printf("Error\n");
 	}
 
+	else
+	{
+		int a = atoi(argv[1]);
+		int b = atoi(argv[2]);
+		int res = a * b;
+
+		printf("%d\n", res);
+	}
+
 	return (1);
+
 }
