75b7710726af1d2623421b59db2cb5d7d65a6308
thra4974 <tristan.thrasher-1@ou.edu>
2020-03-03 15:26:32 -0600

    updated task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -6,17 +6,16 @@
  * create_array - creates array of chars, initializes with specific.
  * @size: unsigned integer represents the size of array.
  * @c: character.
- * Return: NULL if size = 0, Pointer to array if not zero, and NULL if program fails.
+ * Return: NULL if size = 0 or fails, Pointer to array.
  */
 
 char *create_array(unsigned int size, char c)
 {
 	char *arr;
-	unsigned int i = 0;
 
-	arr = malloc(sizeof(char) * size);
+	arr = malloc(size * sizeof(char));
 
-	while (i < size)
+	for (unsigned int i = 0; i < size; i++)
 	{
 		if (size == 0)
 		{
@@ -26,9 +25,9 @@ char *create_array(unsigned int size, char c)
 		else
 		{
 			arr[i] = c;
-			return(arr);
+			return (arr);
 		}
-		i++;
+
 	}
 
 	return (0);
--- 0x0B-malloc_free/0-main.c
+++ 0x0B-malloc_free/0-main.c
@@ -39,13 +39,13 @@ int main(void)
 {
     char *buffer;
 
-    buffer = create_array(20, 'A');
+    buffer = create_array(98, 'A');
     if  (buffer == NULL)
     {
         printf("failed to allocate memory\n");
         return (1);
     }
-    simple_print_buffer(buffer, 20);
+    simple_print_buffer(buffer, 98);
     free(buffer);
     return (0);
 }
