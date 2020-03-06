3337fd782aa780cbf9fa596fc4a7db7fd0b0a74e
thra4974 <tristan.thrasher-1@ou.edu>
2020-03-05 20:58:18 -0600

    updated 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -4,65 +4,53 @@
 
 /**
  * string_nconcat - concatenates two strings.
- * _strlen1 - string length.
- * @str = pointer to string.
  * @s1: pointer to first string.
  * @s2: pointer to 2nd string gets appended to first.
+ * @n: n bytes from s2.
  * Return: pointer to allocated mem. containing s1 and n bytes s2.
  */
 
-unsigned int _strlen1(char *str)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int i = 0;
 
-	while (str[i] != '\0')
+	unsigned int b = 0;
+	unsigned int i = 0;
+	unsigned int j = 0;
+	char *ptr;
+
+	if (!s1)
+	{
+		s1 = "";
+	}
+
+	if (!s2)
+	{
+		s2 = "";
+	}
+
+	while (s1[i])
 	{
 		i++;
 	}
-	return (i);
-}
 
-char *string_nconcat(char *s1, char *s2, unsigned int n)
-{
-	unsigned int len1;
-	unsigned int len2;
-	unsigned int i;
-	unsigned int j;
-	char *ptr;
+	ptr = malloc(((i + n) * sizeof(*s1)) + 1);
 
-	len1 = _strlen1(s1);
-	len2 = _strlen1(s2);
-
-	ptr = (char *)malloc((len1 + len2 + 2) * sizeof(char));
-
-	if (s1 == NULL || s2 == NULL)
-		return (0);
-	else if (n >= len2)
+	if (!ptr)
 	{
-		for (i = 0; s1[i] != '\0'; i++)
-		{
-			ptr[i] = s1[i];
-		}
-		for (j = 0; s2[j] != '\0'; j++, i++)
-		{
-			ptr[i] = s2[j];
-		
-			ptr[i] = '\0';
-		}
-		return (ptr);
-	}
-	else
-	{
-		for (i = 0; s1[i] != '\0'; i++)
-		{
-			s1[i] = ptr[i];
-		}
-		for (j = 0; s2[j] != '\0'; j++, i++)
-		{
-			ptr[i] = s2[j];
-			ptr[i] = '\0';
-		}
-		return (ptr);
+		return (NULL);
 	}
 
+	for (j = 0; j < i; j++)
+	{
+		ptr[j] = s1[j];
+	}
+
+	for (b = 0; b < n && s2[b]; b++, j++)
+	{
+		ptr[j] = s2[b];
+	}
+
+	ptr[j] = '\0';
+
+	return (ptr);
 }
