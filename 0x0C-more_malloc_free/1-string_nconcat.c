efc894fda088d97e75b65aefce9cca30f2727894
thra4974 <tristan.thrasher-1@ou.edu>
2020-03-05 21:00:58 -0600

    updated 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,45 +12,24 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-
 	unsigned int b = 0;
 	unsigned int i = 0;
 	unsigned int j = 0;
 	char *ptr;
 
 	if (!s1)
-	{
 		s1 = "";
-	}
-
 	if (!s2)
-	{
 		s2 = "";
-	}
-
 	while (s1[i])
-	{
 		i++;
-	}
-
 	ptr = malloc(((i + n) * sizeof(*s1)) + 1);
-
 	if (!ptr)
-	{
 		return (NULL);
-	}
-
 	for (j = 0; j < i; j++)
-	{
 		ptr[j] = s1[j];
-	}
-
 	for (b = 0; b < n && s2[b]; b++, j++)
-	{
 		ptr[j] = s2[b];
-	}
-
 	ptr[j] = '\0';
-
 	return (ptr);
 }
