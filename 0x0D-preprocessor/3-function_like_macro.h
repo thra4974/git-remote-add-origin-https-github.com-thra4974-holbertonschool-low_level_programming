27df07acd6507415aa5dba2f5ad823c440c3b14b
thra4974 <tristan.thrasher-1@ou.edu>
2020-03-09 19:59:08 -0500

    updated abso value task

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,21 +1,6 @@
 #ifndef _FUNCTION_LIKE_MACRO_H
 #define _FUNCTION_LIKE_MACRO_H
-/**
- * ABS - returns absolute value of int x.
- * @x: integer input
- * Return: integer
- */
 
-int ABS(int x)
-{
-	if (x < 0)
-	{
-		return ((x) * (-1));
-	}
-	else
-	{
-		return (x);
-	}
-}
+#define ABS(x) ((x) < (0) ? -(x) : (x))
 
 #endif
