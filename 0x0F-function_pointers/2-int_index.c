97094dd01bde2af7db39f08bb18abbbd659fb1d9
thra4974 <tristan.thrasher-1@ou.edu>
2020-03-11 21:16:06 -0500

    task 1 and 2 added

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,33 @@
+#include "function_pointers.h"
+/**
+ * int_index - function searches for an integer
+ * @array: pointer to an array.
+ * @size: number of elements in array.
+ * @cmp: pointer to compare function.
+ * Return: index of first element where cmp doesnt return 0.
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <=0)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) && array[i] != 0)
+		{
+			break;
+		}
+
+		if (array[i] != cmp(array[i]))
+		{
+			continue;
+			return (-1);
+		}
+
+		cmp(array[i]);
+	}
+
+	return (i);
+}
---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,1 @@
+//file contains function that selects the correct function to perform operation asked by user.
