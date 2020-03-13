800e9b75d669cc27269a1a8200a7fa4af3bf2c4d
thra4974 <tristan.thrasher-1@ou.edu>
2020-03-13 17:27:33 -0500

    initialized sum to 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,14 +9,19 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int i;
-	int sum;
+	int sum = 0;
 	va_list numbers;
 
 	va_start(numbers, n);
 
+	if (n == 0)
+	{
+		return (0);
+	}
+	
 	for (i = 0; i < n; i++)
 	{
-		sum += va_arg(numbers, int);
+		sum += va_arg(numbers, unsigned int);
 	}
 
 	va_end(numbers);
