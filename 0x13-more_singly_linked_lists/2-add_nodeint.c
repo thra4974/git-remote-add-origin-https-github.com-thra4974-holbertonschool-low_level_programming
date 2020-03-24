a5c301cc32a62fe6f67d073951ac41e8b0420c47
thra4974 <tristan.thrasher-1@ou.edu>
2020-03-24 11:05:15 -0500

    Formatted 2-add_node file

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -3,19 +3,17 @@
  * add_nodeint - pointer to function adds new node at start
  * @head: double pointer to head of list
  * @n: integer to be added
- * Return: address if pass or NULL if failed 
+ * Return: address if pass or NULL if failed
  */
 
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-
 	listint_t *new = (listint_t *)malloc(sizeof(listint_t));
+
 	if (new == NULL)
 		return (NULL);
-
 	new->n = n;
 	new->next = *head;
 	*head = new;
-
-	return(*head);
+	return (*head);
 }
