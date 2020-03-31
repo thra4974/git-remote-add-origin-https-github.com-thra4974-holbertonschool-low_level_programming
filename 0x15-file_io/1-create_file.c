2df0daa066520ec228233cad9f464ca71ae9b9ee
thra4974 <tristan.thrasher-1@ou.edu>
2020-03-31 15:15:34 -0500

    added TRUNC edge case and return value if text is null

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 	if (filedesc == -1)
 		return (-1);
 	if (text_content == NULL)
-		return (-1);
+		return (1);
 	while (text_content[len] != '\0')
 		len++;
 	wr = write(filedesc, text_content, len);
