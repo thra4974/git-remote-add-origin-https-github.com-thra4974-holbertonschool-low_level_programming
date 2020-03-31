454f17676381b81a082790118e2bcc8df5c8a8ae
thra4974 <tristan.thrasher-1@ou.edu>
2020-03-31 15:13:22 -0500

    added TRUNC edge case

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	filedesc = open(filename, O_CREAT | O_RDWR, 0600);
+	filedesc = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (filedesc == -1)
 		return (-1);
