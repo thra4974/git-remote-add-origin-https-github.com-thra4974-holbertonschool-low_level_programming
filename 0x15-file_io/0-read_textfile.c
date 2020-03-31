a836b6b5d58f5a3589d057eab7297968fd893329
thra4974 <tristan.thrasher-1@ou.edu>
2020-03-31 13:50:15 -0500

    added fail checks and fixed write statement

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,17 +11,41 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 
 	int filedesc;
-	char *buf = malloc(sizeof(size_t) * letters);
+	char *buf;
 	ssize_t rd;
+	ssize_t wr;
+	int cl;
 
-	if (filename == NULL)
+	if (filename == NULL || letters <= 0)
 		return (0);
 	filedesc = open(filename, O_CREAT | O_RDWR);
-	if (filedesc == '\0')
+	if (filedesc == -1)
 		return (0);
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+	{
+		close(filedesc);
+		return (0);
+	}
 	rd = read(filedesc, buf, letters);
-	write(STDOUT_FILENO, buf, letters);
+	if (rd == -1)
+	{
+		free(buf);
+		return (0);
+	}
+	cl = close(filedesc);
+	if (cl == -1)
+	{
+		free(buf);
+		return (0);
+	}
+	wr = write(STDOUT_FILENO, buf, rd);
+	if (wr == -1 || wr != rd)
+	{
+		free(buf);
+		return (0);
+	}
 	close(filedesc);
-	return (rd);
-	return (0);
+	free(buf);
+	return (wr);
 }
