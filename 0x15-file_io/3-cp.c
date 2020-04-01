be874cf4424f3cfa36a6f13bdda1b419ed8dc239
thra4974 <tristan.thrasher-1@ou.edu>
2020-03-31 20:53:28 -0500

    Fixed incompatible pointer error in main function

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,36 +7,49 @@
  * Return: 1 if successful or -1 if function fails
  */
 
-int copy_srcfile_to_target(const char *srcname, const char *targetname, char *text_context)
+int copy_srcfile_to_target(const char *srcname, const char *targetname)
 {
-	int *srcfd, *tgtfd;
+	int srcfd, tgtfd;
 	int clsrc, cltgt;
 	ssize_t wrsrc, rdsrc;
 	char *buf[1024];
 
-	srcfd = open(srcname, O_RDWR, 0554);
+	srcfd = open(srcname, O_RDWR, 0664);
 	tgtfd = open(targetname, O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	//Exit 98; (srcfd, srcname)
 	if (tgtfd == -1)
 		return (-1);
-	
 	while ((rdsrc = read(srcfd, buf, 1024)) > 0)
 	{
 		if (rdsrc == -1)
 		{
-			dprintf(
+			dprintf(STDERR_FILENO, "Error: Can't read from %s\n", srcname);
+			exit(98);
+			return (-1);
 		}
-		wrsrc = write(tgtfd, buffer, rdsrc);
+		wrsrc = write(tgtfd, buf, rdsrc);
 		if (wrsrc == -1)
 		{
-			//close;
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", targetname);
+			exit(99);
 			return (-1);
 		}
 	}
 
-	//exit 98;
 	clsrc = close(srcfd);
+	if (clsrc == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd srcfd");
+		exit(100);
+	}
 	cltgt = close(tgtfd);
+	if (cltgt == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd tgtfd");
+		exit(100);
+	}
+	close(srcfd);
+	close(tgtfd);
+	return (1);
 }
 
 /**
@@ -48,13 +61,13 @@ int copy_srcfile_to_target(const char *srcname, const char *targetname, char *te
 
 int main(int argc, char *argv[])
 {
-	char *srcname, targetname;
+	char *srcname, *targetname;
 	int cpy;
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit (97);
+		exit(97);
 	}
 	srcname = argv[1];
 	targetname = argv[2];
@@ -64,7 +77,7 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", srcname);
 		exit(98);
 	}
-	cpy = copy_srcfile_to_target;
+	cpy = copy_srcfile_to_target(srcname, targetname);
 	if (cpy == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", targetname);
